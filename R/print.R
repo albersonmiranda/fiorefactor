c9361667493104eb55b1f96f1cb3ff8089a21a2a
Alberson Miranda <albersonmiranda@hotmail.com>
2025-08-19 10:06:29 -0300

    add print method for Iom class

---
+++ R/print.R
@@ -0,0 +1,7 @@
+#' Print method for objects of class 'Iom'
+#' @param x An object of class 'Iom'
+#' @param ... Additional arguments passed to print methods
+#' @export
+print.Iom <- function(x, ...) {
+  x$print()
+}
